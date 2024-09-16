d671941403d30abf4c3f408d34ff7b05294c6bbf
Jourzy <emmanora@hotmail.com>
2024-09-16 13:50:31 +0100

    Configured outputs.tf so that all subnet ids are outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-# output "public_subnet_ids" {
-#   description = "ID of the public subnet"
-#   value       = aws_subnet.public_subnets[*].id
-# }
+output "public_subnet_ids" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public[*].id
+}
 
-# output "private_subnet_ids" {
-#   description = "ID of the private subnet"
-#   value       = aws_subnet.private_subnets[*].id
-# }
+output "private_subnet_ids" {
+  description = "ID of the private subnet"
+  value       = aws_subnet.private[*].id
+}
 
